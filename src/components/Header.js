fee5185e6054c6380b160c3de5c023c3a93017f7
Sarah Williams <92603276+Sarahlophus@users.noreply.github.com>
2022-06-26 22:30:59 -0500

    added style and functionality to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,14 @@ const Header = () => {
   return (
     <>
       <header>
-        <nav className="navbar is-fixed-top " role="navigation">
+        <nav className="bgc-midnight navbar is-fixed-top m-4" role="navigation">
           <div className="navbar-brand">
             {/* logo */}
-            <a className="navbar-item" href="#top">
+            {/* <a className="navbar-item" href="#top">
               <img src={sawLogo} alt="an 8-bit avatar of a winking woman with glasses" className="w-xl"></img>
-            </a>
+            </a> */}
+
+            <h1 className="bdr-pink br-15 c-pink has-text-weight-bold is-size-4 p-1">SAW</h1>
 
             {/* navbar burger */}
             <div
@@ -22,31 +24,35 @@ const Header = () => {
               className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
               data-target="navbarMenuToggle"
             >
-              <span></span>
-              <span></span>
-              <span></span>
+              <span className="c-white"></span>
+              <span className="c-white"></span>
+              <span className="c-white"></span>
             </div>
           </div>
           {/* navbar items */}
-          <div id="navbarMenuLinkToggle" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
-            <div className="navbar-start">
-              <a href="#about" class="navbar-item">
+          <div id="navbarMenuLinkToggle" className={`navbar-menu ${isActive ? "bgc-eggplant is-active" : ""}`}>
+            <div className="navbar-end">
+              <a href="#about" class="c-white is-size-5 navbar-item">
                 About
               </a>
 
-              <a href="#projects" className="navbar-item">
+              <a href="#projects" className="c-white is-size-5 navbar-item">
                 Projects
               </a>
 
-              <a href="#experience" className="navbar-item">
+              <a href="#experience" className="c-white is-size-5 navbar-item">
                 Experience
               </a>
 
-              <a href="#contact" className="navbar-item">
+              <a href="#contact" className="c-white is-size-5 navbar-item">
                 Contact
               </a>
 
-              <p className="button navbar-item">Resume</p>
+              <p className="control">
+                <a href="../assets/images/SWilliams-Resume2022.pdf" className="button bgc-pink c-midnight has-text-weight-semibold is-outlined is-rounded navbar-item">
+                  Resume
+                </a>
+              </p>
             </div>
           </div>
         </nav>
